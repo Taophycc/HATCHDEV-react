4e51899cefe873025965f9ddcd95f592ab49c68a
Taofeek Kassim <taophyckassim@gmail.com>
2025-08-07 03:01:01 +0100

    Fix warnings and configure GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function SocialLinks() {
   return (
     <main>
       <div className="card">
-        <img src="avatar-jessica.jpeg" />
+        <img src="avatar-jessica.jpeg" alt="Jessica Randall" />
         <h2>Jessica Randall</h2>
         <h4>London, United Kingdom</h4>
         <p>"Front-end developer and avid reader"</p>
 
         <div className="btns">
-          <a href="#">Github</a>
-          <a href="#">Frontend Mentor</a>
-          <a href="#">LinkedIn</a>
-          <a href="#">Twitter</a>
-          <a href="#">Instagram</a>
+          <a href="https://github.com">Github</a>
+          <a href="https://www.frontendmentor.io">Frontend Mentor</a>
+          <a href="https://www.linkedin.com">LinkedIn</a>
+          <a href="https://twitter.com">Twitter</a>
+          <a href="https://www.instagram.com">Instagram</a>
         </div>
       </div>
     </main>
