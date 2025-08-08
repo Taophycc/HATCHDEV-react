b522c4891f697e781f629a8fd338fbcbe52e7068
Taofeek Kassim <taophyckassim@gmail.com>
2025-08-08 14:30:13 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ function SocialLinks() {
   return (
     <main>
       <div className="card">
-        <img src="avatar-jessica.jpeg" alt="Jessica Randall" />
-        <h2>Jessica Randall</h2>
-        <h4>London, United Kingdom</h4>
-        <p>"Front-end developer and avid reader"</p>
+        <img
+          src="https://pbs.twimg.com/profile_images/1864269288309239808/D-3SqVv0_400x400.jpg"
+          alt="Taofeek Kassim"
+        />
+        <h2>Taofeek Kassim</h2>
+        <h4>San Francisco, CA </h4>
+        <p>"Software Engineer and avid reader"</p>
 
         <div className="btns">
-          <a href="https://github.com">Github</a>
-          <a href="https://www.frontendmentor.io">Frontend Mentor</a>
-          <a href="https://www.linkedin.com">LinkedIn</a>
-          <a href="https://twitter.com">Twitter</a>
+          <a href="https://github.com/Taophycc">Github</a>
+          <a href="https://www.frontendmentor.io/taophycc">Frontend Mentor</a>
+          <a href="https://www.linkedin.com/in/taofeek-kassim/">LinkedIn</a>
+          <a href="https://twitter.com/taophyc_">Twitter</a>
           <a href="https://www.instagram.com">Instagram</a>
         </div>
       </div>
